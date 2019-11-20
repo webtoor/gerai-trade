8d4b780c067594da94c8b309642f92260b975498
Fityan Ali Munshi <fityanalimunshi@gmail.com>
2019-11-20 14:11:00 +0700

    [Admin] Function insert blog

--- app/Http/Controllers/AdminBlogController.php
+++ app/Http/Controllers/AdminBlogController.php
@@ -15,11 +15,31 @@ class AdminBlogController extends Controller
     }
 
     function insert(Request $request){
-        return $data = $request->validate([
+        $data = $request->validate([
             'user_id' => ['required'],
             'judul' => ['required'],
             'konten' => ['required'],
-            'image' => 'required|file|mimes:jpeg,jpg,png|max:5000'
+            'image' => 'required|mimes:jpeg,jpg,png|max:5000'
         ]); 
+        
+        try {
+            $files = $request->file('image');
+            $imageName = 'blog_'.time().Str::random(10).'.png';
+            $path = Storage::disk('public')->putFileAs('blog', $file, $imageName);
+                $post = Blog::create([
+                'user_id' => $data['user_id'],
+                'judul' => $data['judul'],
+                'konten' => $data['konten'],
+                'image' => $imageName
+                ]);
+            $newPost = $post->replicate();
+
+            return redirect()->route('admin-panel.kelola-blog')->withSuccess(trans('Berhasil Menambahkan Cerita'));
+
+        } catch (\Exception $e) {
+            //throw $th;
+        }
+       
+
     }
 }
--- resources/views/admin/blogs/tambahCerita.blade.php
+++ resources/views/admin/blogs/tambahCerita.blade.php
@@ -15,23 +15,23 @@
     <h4 class="c-blue-900"><b>Tambah Cerita</b></h4>
 </div>
 <div class="bgc-white p-20 bd">
-  <form action=" {!! action('AdminProdukController@insert') !!}" method="POST" enctype="multipart/form-data">
+  <form action=" {!! action('AdminBlogController@insert') !!}" method="POST" enctype="multipart/form-data">
   {{ csrf_field() }}
 
    
                 <div class="form-group">
                   <label>Judul Cerita <sup style="color:red"> *Wajib</sup></label>
-                  <input type="text" class="form-control" name="nama_produk" id="nama_produk" aria-describedby="emailHelp" placeholder="Masukan Nama Produk" required>
+                  <input type="text" class="form-control" name="judul" id="judul" aria-describedby="emailHelp" placeholder="Masukan Judul Cerita" required>
                 </div>
                 <div class="form-group">
                         <label>Konten <sup style="color:red"> *Wajib *Hanya Karakter/String</sup> </label>
-                       <textarea  id="deskripsi" name="konten" class="form-control" ></textarea>
+                       <textarea  id="konten" name="konten" class="form-control" required></textarea>
                 </div>
                     <div class="form-group">
                           <label>Pilih Banner Foto<sup style="color:red"> *Wajib </sup></label>
-                          <input type="file" class="form-control" name="image_blog" id="file-input" required/>                            
+                          <input type="file" class="form-control" name="image" id="file-input" required/>                            
                         </div>
-
+                <input type="hidden" value="{{Auth::user()->id}}" name="user_id">
                 <button type="submit" id="submit" class="btn btn-primary">Submit</button>
 
                 </form>
