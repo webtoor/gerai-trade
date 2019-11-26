5f1f131b5bbab319f14f34fe21d722630b287983
Fityan Ali Munshi <fityanalimunshi@gmail.com>
2019-11-26 15:53:01 +0700

    [User] create pengaturan UI

--- resources/views/users/pengaturan.blade.php
+++ resources/views/users/pengaturan.blade.php
@@ -10,30 +10,47 @@
 
     <ul class="nav nav-tabs" id="myTab" role="tablist" style="margin-top:10px;">
             <li class="nav-item">
-              <a class="nav-link active" id="home-tab" data-toggle="tab" href="#wishlist" role="tab" aria-controls="wishlist" aria-selected="true">Wishlist</a>
+              <a class="nav-link active" id="home-tab" data-toggle="tab" href="#wishlist" role="tab" aria-controls="wishlist" aria-selected="true">Alamat Pengiriman</a>
             </li>
           </ul>
           <div class="tab-content" id="myTabContent">
             <div class="tab-pane fade show active" id="wishlist" role="tabpanel" aria-labelledby="wishlist-tab">
 
-                    <div class="row" style="margin-top:50px;">
-                                
-                                   <div class="col-lg-3 col-sm-4">
-                               <div class="single-product">
-                                       <div class="card">
-                                           <div class="view overlay">
-                                                                                                         
-                                               <a href="#">
-                                                           <div class="mask rgba-white-slight"></div>
-                                               </a>
-                                           </div>
-                                           
-                               </div>
-                           </div>
-                       </div>
+                    <div class="row" style="margin-top:40px;">
+                      <div class="col-sm-12">
+                        <a href="#" class="btn btn-primary btn-md" title="{{ trans('Tambah Cerita') }}"><b><i class="fa fa-plus"></i> Tambah Alamat Baru</b></a>
+                        <div class="mT-20">
+                            <table class="table table-bordered" cellspacing="0" width="100%">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th width="150px">Penerima</th>
+                                        <th>Alamat Pengiriman</th>
+                                        <th>Daerah Pengiriman</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                  
+                                    <tr>
+                                        <td></td>
+                                        
+                                        <td> </td>
+                                        <td></td>
+                                        <td></td>
+                                        
+                                      
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                             
                      </div>
                            
-                           
-
+            </div>
+          </div>    
+    </div>
+    </div>       
+</div>
  
 @endsection
