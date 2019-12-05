2c4ab7b8a71b541559246be3f58e25ee65cf9844
Fityan Ali Munshi <fityanalimunshi@gmail.com>
2019-12-05 11:41:55 +0700

    integration rajaOngkir kecamatan

--- app/Helper/rajaongkir.php
+++ app/Helper/rajaongkir.php
@@ -97,6 +97,38 @@ if( !function_exists('city')){
 		}
 	}
 }
+if( !function_exists('kecamatan')){
+	function kecamatan($city_id=null){
+		
+		$uri = "https://pro.rajaongkir.com/api/subdistrict?city=".$city_id;
+		
+		$curl = curl_init();
+
+		 curl_setopt_array($curl, array(
+		  CURLOPT_URL => $uri,
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "GET",
+		  CURLOPT_HTTPHEADER => array(
+		    "key: 87b64ed6c63f0b7b591af87aabf2f6ef"
+		  ),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		  echo "cURL Error #:" . $err;
+		} else {
+		  return $response;
+		}
+	}
+}
 if(!function_exists('cost')){
 	 function cost($origin,$destination,$weight,$courier){
 		$curl = curl_init();
