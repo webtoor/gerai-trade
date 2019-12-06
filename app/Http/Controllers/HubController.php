ea5714f928cf43702aa7897d71e760174447bf2f
Fityan Ali Munshi <fityanalimunshi@gmail.com>
2019-12-06 17:52:07 +0700

    [HUB] Show produk saya

--- app/Http/Controllers/HubController.php
+++ app/Http/Controllers/HubController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Kategori;
-
+use App\Models\Produk;
+use Illuminate\Support\Facades\Auth;
 class HubController extends Controller
 {
     public function index(){
+        $hub_id = Auth::user()->id;
         $kategori = Kategori::with('sub_kategori')->get();
-
-        return view ('users.hub.produkSaya', ['kategori' => $kategori]);
+        $produk = Produk::where('hub_id', $hub_id)->get();
+        return view ('users.hub.produkSaya', ['kategori' => $kategori, 'produk' => $produk]);
     }
 }
--- resources/views/users/hub/produkSaya.blade.php
+++ resources/views/users/hub/produkSaya.blade.php
@@ -15,7 +15,7 @@
     <div class="col-sm-10" style="margin-top:30px;">
     <div class="bgc-white p-10 bd">
 
-    <a href="#" class="btn btn-info btn-md" title="{{ trans('Tambah Produk') }}"><b><i class="fa fa-plus"></i> Cerita Baru</b></a>
+    <a href="#" class="btn btn-dark btn-md" title="{{ trans('Tambah Produk') }}"><b><i class="fa fa-plus"></i> Tambah Produk</b></a>
     <div class="mT-30">
 
     <table id="dataTable" class="table" width="100%">
@@ -29,6 +29,55 @@
                 <th width="100px">Action</th>
             </tr>
         </thead>
+        <tbody>
+                @foreach ($produk as $produks)
+                    
+               
+                 <tr>
+                     <td> 
+                         @if(count($produks->produk_image))
+                         <img src="{{ asset('storage/' .$produks->produk_image[0]->image_path)}}" style="height:150px; width: 150px;">
+                         @else
+                         <img src="http://placehold.it/150x150" style="height:150px; width: 150px;"> 
+                         @endif
+                        {{--  <img src="{{ asset('storage/' .$produks->produk_image[0]->image_path)}}" style="height:150px; width: 150px;"> --}}
+                     </td>
+                     <td>{{$produks->nama_produk}}</td>
+                     <td>{{$produks->stok}}</td>
+                     <td>Rp {{number_format($produks->harga_dasar,0, ".", ".")}}</td>
+                     <td>
+                     @if($produks->status)
+                     Aktif
+                     @else
+                     Non-Aktif
+                     @endif    
+                     </td>
+ 
+                     <td>
+                             <ul class="list-inline">
+                                     <li class="list-inline-item">
+                                         <a href="{{route('admin-panel.edit-produk', ['produk_id' => $produks->id])}}" title="{{ trans('Edit Produk') }}" class="btn btn-info px-3 btn-sm">
+                                             <span class="ti-pencil"></span>
+                                         </a>
+                                     </li>
+                                     <li class="list-inline-item">
+                                             {!! Form::open([
+                                                 'class'=>'delete',
+                                                 'url'  => route('admin-panel.delete-produk', $produks->id), 
+                                                 'method' => 'DELETE',
+                                                 ]) 
+                                             !!}
+                                                 <button class="btn btn-danger px-3 btn-sm" title="{{ trans('app.delete_title') }}"><i class="ti-trash"></i></button>
+                                                 
+                                             {!! Form::close() !!}
+                                         </li>
+                             </ul>
+                       
+                     </td>
+                   
+                 </tr>
+                 @endforeach
+             </tbody>
     </table>
     </div>
 
