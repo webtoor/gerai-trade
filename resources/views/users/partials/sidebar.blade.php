40325893db2cf8c623c860ede77b9d35a84b62e5
Fityan Ali Munshi <fityanalimunshi@gmail.com>
2020-01-06 15:20:07 +0700

    [Hub] Notification pesanan-baru

--- resources/views/users/partials/sidebar.blade.php
+++ resources/views/users/partials/sidebar.blade.php
@@ -35,7 +35,14 @@ use App\Models\Transaction;
             <p href="#" class="list-group-item border-bottom-0">
                 <b style="font-size:14px;">PENJUALAN</b>    
             </p>
-            <a href="{{route('home.get-penjualan')}}" class="list-group-item border-top-0" style="font-size:14px;">Penjualan Saya</a>
+            <a href="{{route('home.get-penjualan')}}" class="list-group-item border-top-0" style="font-size:14px;">Penjualan Saya
+            <?php $hub = Transaction::where(['hub_id' => Auth::user()->id])->whereIn('status_id', ['2'])->orderBy('created_at', 'desc')->get(); ?>
+
+            @if(count($hub) > 0)
+            <span class="badge badge-danger">{{count($hub)}}</span>
+
+            @endif
+            </a>
 
             <p href="#" class="list-group-item border-bottom-0">
                 <b style="font-size:14px;">CERITA</b>    
